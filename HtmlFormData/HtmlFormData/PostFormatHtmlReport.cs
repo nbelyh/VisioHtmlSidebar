456f736e64b8ebe235634624ba0ddadfdd736a8e
Nikolay Belykh <nbelyh@gmail.com>
2018-08-01 00:36:48 +0200

    support links, pictures, and left align

--- HtmlFormData/HtmlFormData/PostFormatHtmlReport.cs
+++ HtmlFormData/HtmlFormData/PostFormatHtmlReport.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net;
+using AngleSharp.Dom.Html;
 using AngleSharp.Extensions;
 using AngleSharp.Parser.Html;
 
@@ -7,10 +9,42 @@ namespace HtmlFormData
 {
     public static class PostFormatHtmlReport
     {
+        private static void FixStyle(StyleParser parser, string selector, string name, string oldVal, string newVal)
+        {
+            var rule = parser
+                .Stylesheet
+                .FirstOrDefault(sel => sel.Name == selector);
+
+            if (rule != null)
+            {
+                var val = rule.Properties
+                    .FirstOrDefault(x => x.Name == name && x.Value == oldVal);
+
+                if (val != null)
+                    val.Value = newVal;
+            }
+        }
+
+        private static void ProcessDocStyles(IHtmlDocument doc)
+        {
+            var parser = new StyleParser();
+
+            var style = doc.QuerySelector("STYLE");
+            
+            parser.Parse(style.TextContent);
+            
+            FixStyle(parser, "TD", "text-align", "right", "left");
+            FixStyle(parser, "TH", "text-align", "right", "left");
+
+            style.TextContent = parser.ToString();
+        }
+
         public static string PostProcess(string html)
         {
             var parser = new HtmlParser();
             var doc = parser.Parse(html);
+
+            ProcessDocStyles(doc);
 
             var rows = doc.QuerySelectorAll("TABLE TR").ToList();
 
